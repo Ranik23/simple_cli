4a053890527dcfa141393879a05c1b2199a7b37e
anton <antonfedorov190@yandex.ru>
2024-10-06 06:08:41 +0300

    initial commit

--- benchmarks/benchmark_test.go
+++ benchmarks/benchmark_test.go
@@ -1,7 +1,6 @@
 package benchmarks
 
 import (
-	"fmt"
 	_ "net/http/pprof"
 	"os"
 	"os/exec"
@@ -9,72 +8,86 @@ import (
 	"testing"
 )
 
-var names = []string{
-    //"test_files/file_1GB.txt",
-    "test_files/file_50MB.txt",
+func BenchmarkCatFile1GB(b *testing.B) {
+    benchmarkCat(b, "test_files/file_1GB.txt")
 }
 
-func BenchmarkCat(b *testing.B) {
+func BenchmarkCatFile50MB(b *testing.B) {
+    benchmarkCat(b, "test_files/file_50MB.txt")
+}
+
+func BenchmarkCountFile1GB(b *testing.B) {
+    benchmarkCount(b, "test_files/file_1GB.txt")
+}
+
+func BenchmarkCountFile50MB(b *testing.B) {
+    benchmarkCount(b, "test_files/file_50MB.txt")
+}
+
+func BenchmarkCountSFile50MB(b *testing.B) {
+    benchmarkCountS(b, "test_files/file_50MB.txt")
+}
+
+func BenchmarkCountSFile1GB(b *testing.B) {
+    benchmarkCountS(b, "test_files/file_1GB.txt")
+}
+
+func benchmarkCat(b *testing.B, filename string) {
     var m runtime.MemStats
     runtime.ReadMemStats(&m)
-    b.Logf("Before reading file: Alloc = %v MiB", m.Alloc/1024/1024)
+    b.Logf("Before reading file: Alloc = %v KiB", m.Alloc/1024)
 
     for i := 0; i < b.N; i++ {
-        for _, filename := range names {
-            cmd := exec.Command("cli", "cat", filename)
+        cmd := exec.Command("cli", "cat", filename)
+        cmd.Env = append(os.Environ(), "CONFIG=../config.yml")
 
-            cmd.Env = append(os.Environ(), "CONFIG=../config.yml")
-
-            output, err := cmd.CombinedOutput() 
-            if err != nil {
-                b.Errorf("Error running command: %v, output: %s", err, output)
-                return
-            }
+        output, err := cmd.CombinedOutput() 
+        if err != nil {
+            b.Errorf("Error running command: %v, output: %s", err, output)
+            return
         }
     }
     runtime.ReadMemStats(&m)
-    b.Logf("After reading file: Alloc = %v MiB", m.Alloc/1024/1024)
+    b.Logf("After reading file: Alloc = %v KiB", m.Alloc/1024)
 }
 
-
-
-func BenchmarkCount(b *testing.B) {
+func benchmarkCount(b *testing.B, filename string) {
     var m runtime.MemStats
     runtime.ReadMemStats(&m)
-    fmt.Printf("Before reading file: Alloc = %v MiB\n", m.Alloc/1024/1024)
-    
-    for _, filename := range names {
+    b.Logf("Before reading file: Alloc = %v KiB", m.Alloc/1024)
+
+    for i := 0; i < b.N; i++ {
         cmd := exec.Command("cli", "count", filename)
 
         cmd.Env = append(os.Environ(), "CONFIG=../config.yml")
 
-        output, err := cmd.CombinedOutput()
+        output, err := cmd.CombinedOutput() 
         if err != nil {
-            b.Errorf("Error running command: %v", string(output))
+            b.Errorf("Error running command: %v, output: %s", err, output)
+            return
         }
     }
-    
     runtime.ReadMemStats(&m)
-    fmt.Printf("After reading file: Alloc = %v MiB\n", m.Alloc/1024/1024)
+    b.Logf("After reading file: Alloc = %v KiB", m.Alloc/1024)
 }
 
-func BenchmarkCountS(b *testing.B) {
+func benchmarkCountS(b *testing.B, filename string) {
     var m runtime.MemStats
     runtime.ReadMemStats(&m)
-    fmt.Printf("Before reading file: Alloc = %v MiB\n", m.Alloc/1024/1024)
-    
-    for _, filename := range names {
+    b.Logf("Before reading file: Alloc = %v KiB", m.Alloc/1024)
+
+    for i := 0; i < b.N; i++ {
         cmd := exec.Command("cli", "countS", filename)
 
         cmd.Env = append(os.Environ(), "CONFIG=../config.yml")
-        
-        output, err := cmd.CombinedOutput()
+
+        output, err := cmd.CombinedOutput() 
         if err != nil {
-            b.Errorf("Error running command: %v", string(output))
+            b.Errorf("Error running command: %v, output: %s", err, output)
+            return
         }
     }
-    
     runtime.ReadMemStats(&m)
-    fmt.Printf("After reading file: Alloc = %v MiB\n", m.Alloc/1024/1024)
+    b.Logf("After reading file: Alloc = %v KiB", m.Alloc/1024)
 }
 
